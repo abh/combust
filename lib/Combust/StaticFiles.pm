b7d7979e56482da3b8e415fa5cc83ff12cd737c2
Ask Bjørn Hansen <ask@develooper.com>
2022-12-05 21:11:24 -0800

    static files: use git blob version for unmodified files also in development

--- lib/Combust/StaticFiles.pm
+++ lib/Combust/StaticFiles.pm
@@ -4,19 +4,23 @@ use List::Util qw(first max);
 use JSON::XS ();
 use Carp qw(cluck croak);
 use Combust::Config;
+use Cwd qw(getcwd);
+use Storable qw(nstore);
+use DBI ();    # for DBI::hash
 
 use namespace::clean;
 
-my $config = Combust::Config->new;
-my $startup_time = time;
-my $static_file_paths = {}; 
-my $json = JSON::XS->new->relaxed(1);
+my $config            = Combust::Config->new;
+my $startup_time      = time;
+my $static_file_paths = {};
+my $json              = JSON::XS->new->relaxed(1);
 
 my %singletons;
 
 sub new {
     my $proto = shift;
-    my %args = (
+    my %args  = (
+
         # defaults go here ...
         @_,
     );
@@ -27,6 +31,11 @@ sub new {
     return $singletons{$args{site}} if $args{site} and $singletons{$args{site}};
 
     my $self = bless \%args, $proto;
+
+    # in development we always rebuild the data
+    if ($self->deployment_mode eq 'devel') {
+        $self->build();
+    }
 
     unless ($self->{setup}) {
         my @sites = $config->sites_list;
@@ -49,10 +58,8 @@ sub find_static_path {
     my ($self, $site) = @_;
     my $root_dir = $config->root_docs;
 
-    my @static_dirs = ($root_dir . "/$site/static",
-                       $root_dir . "/static",
-                       $root_dir . "/shared/static",
-                      );
+    my @static_dirs =
+      ($root_dir . "/$site/static", $root_dir . "/static", $root_dir . "/shared/static",);
     return first { -e $_ && -d _ } @static_dirs;
 }
 
@@ -64,19 +71,18 @@ sub setup_static_files {
 
     $static_file_paths->{$site}->{path} = $static_directory;
 
-    my $static_files = 
-        eval { retrieve("${static_directory}/.static.versions.store") }
-        || $self->_load_json("${static_directory}/.static.versions.json");
+    my $static_files = eval { retrieve("${static_directory}/.static.versions.store") }
+      || $self->_load_json("${static_directory}/.static.versions.json");
 
     # TODO: in devel deployment mode we should reload this
     # automatically when the .json file changes
     my $static_groups_file = "${static_directory}/.static.groups.json";
-    my $static_groups = -r $static_groups_file && $self->_load_json($static_groups_file) || {};
+    my $static_groups      = -r $static_groups_file && $self->_load_json($static_groups_file) || {};
 
     # no relative filenames in the groups
     for my $name (keys %$static_groups) {
         my $group = $static_groups->{$name};
-        $group->{files} = [ map { $_ =~ m!^/! ? $_ : "/$_"  } @{ $group->{files} } ];
+        $group->{files} = [map { $_ =~ m!^/! ? $_ : "/$_" } @{$group->{files}}];
     }
 
     $static_file_paths->{$site}->{groups} = $static_groups;
@@ -86,12 +92,11 @@ sub setup_static_files {
 sub _load_json {
     my ($self, $file) = @_;
     return {} unless -r $file;
-    my $data = 
-        eval { 
-            local $/ = undef;
-            open my $fh, $file or die "Could not open $file: $!";
-            my $versions = <$fh>;
-            return $json->decode($versions)
+    my $data = eval {
+        local $/ = undef;
+        open my $fh, $file or die "Could not open $file: $!";
+        my $versions = <$fh>;
+        return $json->decode($versions);
     };
     warn $@ if $@;
     return $data;
@@ -128,13 +133,12 @@ sub static_base_ssl {
     $base;
 }
 
-
 sub static_group {
     my ($self, $name) = @_;
     my $data = $self->static_group_data($name);
     return unless $data;
-    return "/.g/$name" if $self->deployment_mode ne 'devel'; 
-    return @{ $data->{files} };
+    return "/.g/$name" if $self->deployment_mode ne 'devel';
+    return @{$data->{files}};
 }
 
 sub static_group_data {
@@ -144,13 +148,13 @@ sub static_group_data {
 }
 
 sub static_groups {
-    my $self = shift;
+    my $self   = shift;
     my $groups = $self->static_file_paths->{groups};
     return () unless $groups;
     return sort keys %$groups;
 }
 
-sub site { return shift->{site} } 
+sub site { return shift->{site} }
 
 sub static_url {
     my ($self, $file) = @_;
@@ -165,7 +169,21 @@ sub static_url {
         my $version;
         if ($self->deployment_mode eq 'devel') {
             my $static_directory = $static_files->{path};
-            $version = max($startup_time, (stat("${static_directory}$file"))[9]);
+
+            #warn "STAT: ${static_directory}$file - ", (stat("${static_directory}$file"))[9]
+            #  if $file =~ m/graphs.server/;
+
+            my $build_time = (@{$static_files->{files}->{"..build.time"}})[0];
+            my $file_time  = (stat("${static_directory}$file"))[9];
+
+            if ($build_time > $file_time) {
+                if (ref $static_files->{files}->{$file}) {
+                    ($version, $file_attr) = (@{$static_files->{files}->{$file}});
+                }
+            }
+            if (!$version or $version =~ /^M-/) {
+                $version = max($startup_time, $file_time);
+            }
         }
         elsif (ref $static_files->{files}->{$file}) {
             ($version, $file_attr) = (@{$static_files->{files}->{$file}});
@@ -181,6 +199,129 @@ sub static_url {
     return $self->static_base($self->site) . $file;
 }
 
+sub build {
+    my $static = shift;
+    my $config = Combust::Config->new;
 
+    my $root     = $ENV{CBROOTLOCAL};
+    my $root_dir = $config->root_docs;
+
+    my @sites = $config->sites_list;
+
+    my %paths_done;
+
+    for my $site (@sites) {
+        my $now = time;
+        my $dir = $static->find_static_path($site);
+        next unless $dir;
+        next if $paths_done{$dir}++;
+
+        my $deployment_mode = $config->site->{$site}->{deployment_mode} || 'test';
+
+        my $storable_file = "$dir/.static.versions.store";
+        my $json_file     = "$dir/.static.versions.json";
+        if (-e $json_file && !-w $json_file) {
+
+            # it's not clear why this is a good idea; probably
+            # the deployment script should decide if it wants
+            # to run this or not ...
+            warn "$json_file not writable, skipping\n";
+            next;
+        }
+
+        chdir $dir or die "could not chdir to $dir";
+
+        my $files = {};
+
+        if (-d "$root/.svn") {
+            my $svn   = `svn info -R`;
+            my @files = map {
+                +{map { chomp; split /: /, $_, 2 } split /\n/}
+            } split /\n\n/, $svn;
+            for my $file (@files) {
+                $files->{"/" . $file->{Path}} = $file->{"Last Changed Rev"};
+            }
+        }
+        elsif (-d "$root/.git") {
+            $files = _get_git_tree($root, $dir);
+        }
+        else {
+            warn "Could not find .svn or .git directory\n" if $deployment_mode eq 'devel';
+            exit;
+        }
+
+        my $extra = $static->_load_json('.static.versions.extra.json')
+          if -e '.static.versions.extra.json';
+        $files = {%$files, %$extra} if $extra;
+
+        #print Data::Dumper->Dump([\$files], [qw(files)]);
+
+        for my $k (keys %$files) {
+            my $version = $files->{$k} || 0;
+
+            my $server       = DBI::hash("$k/$version", 1) + 0;
+            my $server_short = ($server % 2) + 1;
+            my $attr         = {
+                server     => $server_short,
+                server_num => $server % 100,
+            };
+            if ($k =~ m/(?<!-min)\.(css|js)/) {
+                my $min = $k;
+                $min =~ s/(\.[^\.]+)$/-min$1/;
+                if (-f "$dir/$min") {
+                    $attr->{min} = 1;
+                }
+            }
+
+            $files->{$k} = [$version, $attr];
+        }
+
+        $files->{"..build.time"} = [$now, {}];
+
+        nstore($files, $storable_file) or die "could not store $storable_file: $!";
+        $static->_save_json($json_file, $files);
+    }
+}
+
+sub _get_git_tree {
+    my ($root, $dir) = @_;
+
+    my $old_dir = getcwd();
+    chdir("$root/$dir");
+
+    my $command = "git ls-tree --abbrev -r HEAD $dir";
+    my $git     = `$command`;
+    my @submodules;
+
+    my $files = {
+        map {
+            chomp;
+            my ($type, $version, $file) = (split /\s/, $_)[1, 2, 3];
+            push @submodules, $file if $type eq 'commit';
+            ("/$file" => $version);
+        } split /\n/,
+        $git
+    };
+
+    my $git_status =
+      `git status --porcelain=v2 --no-renames --ignore-submodules=none  --untracked-files=all $dir`;
+    for (split /\n/, $git_status) {
+        chomp;
+        my ($file) = (split /\s/, $_)[-1];
+        if (my $v = $files->{"/$file"}) {
+            $files->{"/$file"} = "M-$v";
+        }
+    }
+
+    for my $submodule (@submodules) {
+        next if $submodule eq $root;    # prevent recursion
+        my $sub = _get_git_tree($submodule, '.');
+        $sub   = {map { ("/$submodule$_" => $sub->{$_}) } keys %$sub};
+        $files = {%$files, %$sub};
+    }
+
+    chdir($old_dir);
+    return $files;
+}
 
 1;
